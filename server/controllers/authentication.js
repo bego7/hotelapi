0e76651e7c01c10472806e9009343eed824645ce
bego <begomontes7@gmail.com>
2019-05-05 12:39:29 -0500

    Fixed api/login route in order to get username and password insted of the id

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -7,14 +7,14 @@ const jwt = require('../services/jwt');
 module.exports = {
     login(req, res) {
         // check that params are not null, undefined or empty string
-        if(!req.body.id || !req.body.contra){ 
+        if(!req.body.nombre_usuario || !req.body.contra){ 
             return res.status(400).send({message: 'The post body must contain a user_id and password field.'});
         }
         
-        let administrador_id = (String)(req.body.id);
+        let nombre_usuario = req.body.nombre_usuario;
         let password = req.body.contra;
 
-        administrador.findByPk(administrador_id)
+        administrador.findOne({where:{nombre_usuario:req.body.nombre_usuario}})
         .then(administrador => {
             if(!administrador){
                 return res.status(400).send({ message: 'Authentication failed. User not found.'});
@@ -23,7 +23,7 @@ module.exports = {
             // if(!user.confirmed){
             //     return res.status(400).send({ message: 'Authentication failed. The account is not confirmed, check your email to confirm your account.'});
             // }
-
+            console.log(administrador.contra);
             bcrypt.compare(password, administrador.contra,(error, check) => {
                 if(check){
                     // devolver miembro y token
