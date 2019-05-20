4030145d5b41c19a39779d4f8209cbccab930df6
Benny Ruiz Jimenez <A01328177@itesm.mx>
2019-05-20 14:36:53 -0500

    Updates, Correcion en Mostrar Habitacion

--- server/controllers/reserva.js
+++ server/controllers/reserva.js
@@ -1,283 +1,298 @@
-const { reserva, pago,cliente,habitacion, tipo } = require('../models');
+const { reserva, pago, cliente, habitacion, tipo } = require('../models');
 const Sequelize = require('sequelize')
 const Op = Sequelize.Op;
 
 // var datetime = new Date(year, month, day);
 // moment().format('YYYY MM DD');
 
- var today =new Date();
- 
+var today = new Date();
+
 module.exports = {
-  create(req, res) {
-    return reserva
-      .create({
-        fecha_ingreso: req.body.fecha_ingreso,
-        fecha_salida:req.body.fecha_salida,
-        cantidad_personas:req.body.cantidad_personas,
-        codigo_reserva:req.body.codigo_reserva,
-        cliente_id:req.body.cliente_id,
-        pago_id:req.body.pago_id
-      })
-      .then(reserva => res.status(201).send(reserva))
-      .catch(error => res.status(400).send(error));
-  },
+    create(req, res) {
+        return reserva
+            .create({
+                fecha_ingreso: req.body.fecha_ingreso,
+                fecha_salida: req.body.fecha_salida,
+                cantidad_personas: req.body.cantidad_personas,
+                codigo_reserva: req.body.codigo_reserva,
+                cliente_id: req.body.cliente_id,
+                pago_id: req.body.pago_id,
+                habitacion_id: req.body.habitacion_id
+            })
+            .then(reserva => res.status(201).send(reserva))
+            .catch(error => res.status(400).send(error));
+    },
 
-  list(req, res) {
-   
-    return reserva
-      .findAll({
-        include: [
-          {
-            model: pago,
-            as: 'idPago',
-            required: false,
-          },
-          {
-            model: cliente,
-            as: 'idCliente',
-            required: false,
-          },
-          {
-            model: habitacion,
-            as:'idHabitacion',
-            required:false,
-            
-          }
-         
-        ],
-      })
-      .then(reserva => res.status(200).send(reserva))
-      .catch(error => res.status(400).send(error));
-  },
+    list(req, res) {
 
-  obtenerHabitaciones(req,res){
+        return reserva
+            .findAll({
+                include: [{
+                        model: pago,
+                        as: 'idPago',
+                        required: false,
+                    },
+                    {
+                        model: cliente,
+                        as: 'idCliente',
+                        required: false,
+                    },
+                    {
+                        model: habitacion,
+                        as: 'idHabitacion',
+                        required: false,
+
+                    }
+
+                ],
+            })
+            .then(reserva => res.status(200).send(reserva))
+            .catch(error => res.status(400).send(error));
+    },
+
+    obtenerHabitaciones(req, res) {
         return reserva.findAll({
-          include: [
-            {
-              model: habitacion,
-              
-            }],
-            
+                include: [{
+                    model: habitacion,
 
-        })
-        .then(reserva => res.status(200).send(reserva))
-        .catch(error => res.status(400).send(error));
-        
+                }],
 
-  },
 
-  disponibles2(req, res) {
-    // check that params are not null, undefined or empty string
-    if(!req.body.fecha_ingreso || !req.body.fecha_salida){ 
-        return res.status(400).send({message: 'Falta fecha de entrada y fecha de salida'});
-    }
-    
-    let fecha_ingreso = req.body.fecha_ingreso;
-    let fecha_salida = req.body.fecha_salida;
+            })
+            .then(reserva => res.status(200).send(reserva))
+            .catch(error => res.status(400).send(error));
 
-    reserva.findAll({
-      include: [
-          {
-            model: pago,
-            as: 'idPago',
-            required: false,
-          },
-          {
-            model: cliente,
-            as: 'idCliente',
-            required: false,
-          },
-          {
-            model: habitacion,
-            as:'idHabitacion',
-            required:false,
-            
-          },
-        ],
-      where:{
-        fecha_ingreso:{
-          [Op.notBetween]:[fecha_ingreso,fecha_salida]
-        },
-        fecha_salida:{
-          [Op.notBetween]:[fecha_ingreso,fecha_salida]
-        }
-      }   
-    })
-    .then(reserva => {
-        if(!reserva){
-            return res.status(400).send({ message: 'No se encontró reserva para esa fecha'});
-        }
-        else{
-          res.status(200).send(reserva);
+
+    },
+
+    disponibles2(req, res) {
+        // check that params are not null, undefined or empty string
+        if (!req.body.fecha_ingreso || !req.body.fecha_salida) {
+            return res.status(400).send({ message: 'Falta fecha de entrada y fecha de salida' });
         }
 
-        // if(!user.confirmed){
-        //     return res.status(400).send({ message: 'Authentication failed. The account is not confirmed, check your email to confirm your account.'});
-        // }
-       
-    })
-    .catch( error => res.status(400).send({ message: 'Request error.' }));
-},
+        let fecha_ingreso = req.body.fecha_ingreso;
+        let fecha_salida = req.body.fecha_salida;
 
-disponibles3(req, res) {
-  // check that params are not null, undefined or empty string
-  if(!req.body.fecha_ingreso || !req.body.fecha_salida){ 
-      return res.status(400).send({message: 'Falta fecha de entrada y fecha de salida'});
-  }
-  
-  let fecha_ingreso = req.body.fecha_ingreso;
-  let fecha_salida = req.body.fecha_salida;
+        reserva.findAll({
+                include: [{
+                        model: pago,
+                        as: 'idPago',
+                        required: false,
+                    },
+                    {
+                        model: cliente,
+                        as: 'idCliente',
+                        required: false,
+                    },
+                    {
+                        model: habitacion,
+                        as: 'idHabitacion',
+                        required: false,
 
-  reserva.findAll({
-    include: [
-        {
-          model: habitacion,
-          as:'idHabitacion',
-          required:false,
-          
-        },
-      ],
-    where:{
-      fecha_ingreso:{
-        [Op.between]:[fecha_ingreso,fecha_salida]
-      },
-      fecha_salida:{
-        [Op.between]:[fecha_ingreso,fecha_salida]
-      }
-    }   
-  })
-  .then(reserva => {
-      if(!reserva){
-        return habitacion.findAll({
-          include: [
-            {
-              model: tipo,
-              as: 'idTipo',
-              required: false,
-            },
-          ],
-        })
-        .then(habitacion => res.status(200).send(habitacion))
-        .catch(error => res.status(400).send(error));
-      }
-      else{
-        return habitacion.findAll({
-          include: [
-            {
-              model: tipo,
-              as: 'idTipo',
-              required: false,
-            },
-          ],
-        })
-        .then(habitacion => res.status(200).send(habitacion))
-        .catch(error => res.status(400).send(error));
-      }
-     
-  })
-  .catch(error => res.status(400).send({ message: 'Request error.' }));
-},
+                    },
+                ],
+                where: {
+                    fecha_ingreso: {
+                        [Op.notBetween]: [fecha_ingreso, fecha_salida]
+                    },
+                    fecha_salida: {
+                        [Op.notBetween]: [fecha_ingreso, fecha_salida]
+                    }
+                }
+            })
+            .then(reserva => {
+                if (!reserva) {
+                    return res.status(400).send({ message: 'No se encontró reserva para esa fecha' });
+                } else {
+                    res.status(200).send(reserva);
+                }
 
-actuales(req,res){
-reserva.findAll({
-  include: [
-    {
-      model: pago,
-      as: 'idPago',
-      required: false,
+                // if(!user.confirmed){
+                //     return res.status(400).send({ message: 'Authentication failed. The account is not confirmed, check your email to confirm your account.'});
+                // }
+
+            })
+            .catch(error => res.status(400).send({ message: 'Request error.' }));
     },
-    {
-      model: cliente,
-      as: 'idCliente',
-      required: false,
+
+    disponibles3(req, res) {
+        // check that params are not null, undefined or empty string
+        if (!req.body.fecha_ingreso || !req.body.fecha_salida) {
+            return res.status(400).send({ message: 'Falta fecha de entrada y fecha de salida' });
+        }
+
+        let fecha_ingreso = req.body.fecha_ingreso;
+        let fecha_salida = req.body.fecha_salida;
+
+        reserva.findAll({
+                include: [{
+                    model: habitacion,
+                    as: 'idHabitacion',
+                    required: false,
+
+                }, ],
+                where: {
+                    fecha_ingreso: {
+                        [Op.between]: [fecha_ingreso, fecha_salida]
+                    },
+                    fecha_salida: {
+                        [Op.between]: [fecha_ingreso, fecha_salida]
+                    }
+                }
+            })
+            .then(reserva => {
+                if (!reserva) {
+                    return habitacion.findAll({
+                            include: [{
+                                model: tipo,
+                                as: 'idTipo',
+                                required: false,
+                            }, ],
+                        })
+                        .then(habitacion => res.status(200).send(habitacion))
+                        .catch(error => res.status(400).send(error));
+                } else {
+                    return habitacion.findAll({
+                            include: [{
+                                model: tipo,
+                                as: 'idTipo',
+                                required: false,
+                            }, ],
+                        })
+                        .then(habitacion => res.status(200).send(habitacion))
+                        .catch(error => res.status(400).send(error));
+                }
+
+            })
+            .catch(error => res.status(400).send({ message: 'Request error.' }));
     },
-    {
-      model: habitacion,
-      as:'idHabitacion',
-      required:false,
-      
+
+    actuales(req, res) {
+        reserva.findAll({
+                include: [{
+                        model: pago,
+                        as: 'idPago',
+                        required: false,
+                    },
+                    {
+                        model: cliente,
+                        as: 'idCliente',
+                        required: false,
+                    },
+                    {
+                        model: habitacion,
+                        as: 'idHabitacion',
+                        required: false,
+
+                    },
+                ],
+                where: {
+                    fecha_ingreso: {
+                        [Op.eq]: today
+                    }
+                }
+
+            })
+            .then(reserva => {
+                if (!reserva) {
+                    return res.status(400).send({ message: 'No se encontró reserva para esa fecha' });
+                } else {
+                    res.status(200).send(reserva);
+                }
+
+                // if(!user.confirmed){
+                //     return res.status(400).send({ message: 'Authentication failed. The account is not confirmed, check your email to confirm your account.'});
+                // }
+
+            })
+            .catch(error => res.status(400).send({ message: 'Request error.' }));
+
     },
-  ],
-  where:{
-  fecha_ingreso:{
-    [Op.eq]:today}
-  }
- 
-})
-.then(reserva => {
-    if(!reserva){
-        return res.status(400).send({ message: 'No se encontró reserva para esa fecha'});
-    }
-    else{
-      res.status(200).send(reserva);
-    }
 
-    // if(!user.confirmed){
-    //     return res.status(400).send({ message: 'Authentication failed. The account is not confirmed, check your email to confirm your account.'});
-    // }
-   
-})
-.catch( error => res.status(400).send({ message: 'Request error.' }));
+    obtenercliente(req, res) {
+        reserva.findAll({
+                include: [{
+                        model: pago,
+                        as: 'idPago',
+                        required: false,
+                    },
+                    {
+                        model: cliente,
+                        as: 'idCliente',
+                        required: false,
+                    },
+                ],
+                where: {
+                    fecha_ingreso: {
+                        [Op.eq]: today
+                    }
+                }
+            })
+            .then(reserva => {
+                if (!reserva) {
+                    return res.status(400).send({ message: 'No se encontró reserva para esa fecha' });
+                } else {
+                    return res.status(200).send(reserva);
+                }
 
-},
+            })
+            .catch(error => res.status(400).send({ message: 'Request error.' }));
 
-obtenercliente(req,res){
-  reserva.findAll({
-    include: [
-      {
-        model: pago,
-        as: 'idPago',
-        required: false,
-      },
-      {
-        model: cliente,
-        as: 'idCliente',
-        required: false,
-      },
-    ],
-    where:{
-      fecha_ingreso:{
-        [Op.eq]:today}
-      }
-  })
-  .then(reserva => {
-    if(!reserva){
-      return res.status(400).send({ message: 'No se encontró reserva para esa fecha'});
-  }
+    },
 
-  else{
-    return  res.status(200).send(reserva);
-  }  
-  
-  })
-  .catch( error => res.status(400).send({ message: 'Request error.' }));
- 
-},
-
-delete(req, res) {
-  return reserva
-  .findByPk(req.params.id)
-  .then((reserva) => {
-    if(!reserva) {
-      return res.status(404).json({
-        message: 'reserva not found'
-      });
-    }
-    return reserva
-    .destroy()
-    .then(() => {
-      res.status(204).send();
-    })
-    .catch((error) => {
-      res.status(400).send(error);
-    });
-  }).catch((error) => {
-    res.status(400).send(error);
-  });
-}
+    delete(req, res) {
+        return reserva
+            .findByPk(req.params.id)
+            .then((reserva) => {
+                if (!reserva) {
+                    return res.status(404).json({
+                        message: 'reserva not found'
+                    });
+                }
+                return reserva
+                    .destroy()
+                    .then(() => {
+                        res.status(204).send();
+                    })
+                    .catch((error) => {
+                        res.status(400).send(error);
+                    });
+            }).catch((error) => {
+                res.status(400).send(error);
+            });
+    },
 
 
+    update(req, res) {
+        return reserva
+            .findByPk(req.params.id, {
+                attributes: ['id', 'fecha_ingreso', 'fecha_salida', 'cantidad_personas', 'codigo_reserva', 'cliente_id', 'pago_id', 'habitacion_id'],
+            })
+            .then((reserva) => {
+                if (!reserva) {
+                    return res.status(400).send({
 
+                        status: 400,
+                        message: 'No reserva with that ID was found.',
+                    });
+                }
+
+                return reserva
+                    .update({
+                        fecha_ingreso: req.body.fecha_ingreso,
+                        fecha_salida: req.body.fecha_salida,
+                        cantidad_personas: req.body.cantidad_personas,
+                        codigo_reserva: req.body.codigo_reserva,
+                        cliente_id: req.body.cliente_id,
+                        pago_id: req.body.pago_id,
+                        habitacion_id: req.body.habitacion_id
+                    })
+                    .then(reserva => res.status(200).send(reserva))
+                    .catch(error => res.status(400).send(error));
+            })
+            .catch(error => res.status(400).send(error));
+    },
 
 
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,60 +6,59 @@ const reservaController = require('../controllers').reserva;
 const administradorController = require('../controllers').administrador;
 const authenticationController = require('../controllers').authentication;
 module.exports = (app) => {
-  app.get('/api', (req, res) => res.status(200).send({
-    message: 'Welcome to the Hotel boutique Ex-hacienda de San Antonio Api!',
-  }));
+    app.get('/api', (req, res) => res.status(200).send({
+        message: 'Welcome to the Hotel boutique Ex-hacienda de San Antonio Api!',
+    }));
 
-  // authentication routes
-  app.post('/api/login', authenticationController.login);
+    // authentication routes
+    app.post('/api/login', authenticationController.login);
 
-  // Routes for clientes
-  app.post('/api/clientes', clienteController.create);
-  app.get('/api/clientes', clienteController.list);
-  app.get('/api/clientes/:id', clienteController.retrieve);
-  app.put('/api/clientes/update/:id', clienteController.update);
+    // Routes for clientes
+    app.post('/api/clientes', clienteController.create);
+    app.get('/api/clientes', clienteController.list);
+    app.get('/api/clientes/:id', clienteController.retrieve);
+    app.put('/api/clientes/update/:id', clienteController.update);
 
-   // Routes for pagos
-  app.post('/api/pagos', pagoController.create);
-  app.get('/api/pagos', pagoController.list);
-  app.get('/api/pagos/:id', pagoController.retrieve);
+    // Routes for pagos
+    app.post('/api/pagos', pagoController.create);
+    app.get('/api/pagos', pagoController.list);
+    app.get('/api/pagos/:id', pagoController.retrieve);
 
-  // Routes for tipos
-  app.post('/api/tipos', tipoController.create);
-  app.get('/api/tipos', tipoController.list);
-  app.get('/api/tipos/:id', tipoController.retrieve);
-  app.put('/api/tipos/update/:id', tipoController.update);
-  app.delete('/api/tipos/delete/:id', tipoController.delete);
+    // Routes for tipos
+    app.post('/api/tipos', tipoController.create);
+    app.get('/api/tipos', tipoController.list);
+    app.get('/api/tipos/:id', tipoController.retrieve);
+    app.put('/api/tipos/update/:id', tipoController.update);
+    app.delete('/api/tipos/delete/:id', tipoController.delete);
 
-  // Routes for habitaciones
-  app.post('/api/habitaciones', habitacionController.create);
-  app.get('/api/habitaciones', habitacionController.list);
-  app.get('/api/habitaciones/:id', habitacionController.retrieve);
-  app.put('/api/habitaciones/update/:id', habitacionController.update);
-  app.delete('/api/habitaciones/delete/:id', habitacionController.delete);
-  app.get('/api/habitaciones/disponibles/:date', habitacionController.available);
+    // Routes for habitaciones
+    app.post('/api/habitaciones', habitacionController.create);
+    app.get('/api/habitaciones', habitacionController.list);
+    app.get('/api/habitaciones/:id', habitacionController.retrieve);
+    app.put('/api/habitaciones/update/:id', habitacionController.update);
+    app.delete('/api/habitaciones/delete/:id', habitacionController.delete);
+    app.get('/api/habitaciones/disponibles/:date', habitacionController.available);
 
 
-// Routes for reservas
-  app.post('/api/reservas', reservaController.create);
-  app.get('/api/reservas', reservaController.list);
-  app.get('/api/reservas/disponibles2', reservaController.disponibles2);
-  app.get('/api/reservas/disponibles3', reservaController.disponibles3);
-  app.get('/api/reservas/actuales', reservaController.actuales);
-  app.get('/api/reservas/habitacions', reservaController.obtenerHabitaciones);
-  app.get('/api/reservas/clientes', reservaController.obtenercliente);
+    // Routes for reservas
+    app.post('/api/reservas', reservaController.create);
+    app.get('/api/reservas', reservaController.list);
+    app.get('/api/reservas/disponibles2', reservaController.disponibles2);
+    app.get('/api/reservas/disponibles3', reservaController.disponibles3);
+    app.get('/api/reservas/actuales', reservaController.actuales);
+    app.get('/api/reservas/habitacions', reservaController.obtenerHabitaciones);
+    app.get('/api/reservas/clientes', reservaController.obtenercliente);
+    app.put('/api/reservas/update/:id', reservaController.update);
+    app.delete('/api/reservas/delete/:id', reservaController.delete);
+    // app.get('/api/reservas/:id', reservaController.retrieve);
+    // app.put('/api/reservas/update/:id', reservaController.update);
 
-  app.delete('/api/reservas/delete/:id', reservaController.delete);
-  // app.get('/api/reservas/:id', reservaController.retrieve);
-  // app.put('/api/reservas/update/:id', reservaController.update);
 
-  
-// Routes for admins
-app.post('/api/administradores', administradorController.create);
-app.get('/api/administradores', administradorController.list);
-app.get('/api/administradores/:id', administradorController.retrieve);
-app.put('/api/administradores/update/:id', administradorController.update);
-app.delete('/api/administradores/delete/:id', administradorController.delete);
-
+    // Routes for admins
+    app.post('/api/administradores', administradorController.create);
+    app.get('/api/administradores', administradorController.list);
+    app.get('/api/administradores/:id', administradorController.retrieve);
+    app.put('/api/administradores/update/:id', administradorController.update);
+    app.delete('/api/administradores/delete/:id', administradorController.delete);
 
 };
